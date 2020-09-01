473674b3c13c9a1566f3fda8a0da6648d49f2604
Atharva Lele <itsatharva@gmail.com>
2020-09-01 23:44:56 +0530

    1_SysTick_Timer: implement SysTick timer and add delay functions using SysTick
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- 1_SysTick_Timer/src/main.c
+++ 1_SysTick_Timer/src/main.c
@@ -1,6 +1,13 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/flash.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+
+#include "../include/main.h"
+
+// 1us ticks
+volatile uint32_t us_ticks = 0;
 
 static void Clock_config(void) {
     rcc_osc_on(RCC_HSI16);
@@ -25,8 +32,42 @@ static void Clock_config(void) {
 	rcc_apb2_frequency = 80e6;
 }
 
+void systick_config(void)
+{
+    systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+
+    systick_set_frequency(1000000, 80e6);
+
+    systick_interrupt_enable();
+
+    systick_counter_enable();
+}
+
+void sys_tick_handler(void)
+{
+    /* Increment counter necessary in delay()*/
+    us_ticks++;
+}
+
+void delay_us(uint32_t dly_ticks)
+{
+      uint32_t cur_ticks;
+ 
+      cur_ticks = us_ticks;
+      while ((us_ticks - cur_ticks) < dly_ticks) ;
+}
+
+void delay_ms(uint32_t ms_count)
+{
+    uint32_t i;
+    for (i = 0; i < ms_count; i++) {
+        delay_us(1000);
+    }
+}
+
 int main(void) {
     Clock_config();
+    systick_config();
 
     gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO2);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
@@ -34,8 +75,7 @@ int main(void) {
     while(1) {
         gpio_toggle(GPIOB, GPIO2);
 
-        for (int i = 0; i < 4000000; i++)
-            __asm__("NOP"); 
+        delay_ms(1000);
 
         gpio_toggle(GPIOE, GPIO8);
     }
