cbc05bd2c558de4c37e04a16af022b1813b7c9b7
Ayush <ayush.sriv79@gmail.com>
2020-09-01 18:46:44 +0530

    1_SysTick_TImer: First Commit Added Clock_config for system and peripherals

---
+++ 1_SysTick_Timer/src/main.c
@@ -0,0 +1,36 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+static void Clock_config(void) {
+    rcc_osc_on(RCC_HSI16);
+
+    rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 4, 40, 0, 0, RCC_PLLCFGR_PLLQ_DIV2);
+    rcc_osc_on(RCC_PLL);
+
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_GPIOE);
+
+    rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
+    rcc_wait_for_sysclk_status(RCC_PLL);
+
+    rcc_ahb_frequency = 80e6;
+	rcc_apb1_frequency = 80e6;
+	rcc_apb2_frequency = 80e6;
+}
+
+int main(void) {
+    Clock_config();
+
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO2);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
+
+    while(1) {
+        gpio_toggle(GPIOB, GPIO2);
+
+        for (int i = 0; i < 4000000; i++)
+            __asm__("NOP");
+        
+        gpio_toggle(GPIOE, GPIO8);
+    }
+    return 0;
+}
